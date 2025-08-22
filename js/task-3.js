cb258caa2781fc2689568880f088204a04c5bbad
andrei_gubsky <anrei.gubsky@gmail.com>
2025-08-23 01:03:05 +0300

    Prepare task 3 and 4

---
+++ js/task-3.js
@@ -0,0 +1,84 @@
+const sortByDescendingFriendCount = (users) => {
+    //масив усіх користувачів, відсортованих за спаданням 
+    //кількостій їх друзів (властивість friends)
+    let sortedByAscendingFriends = users.toSorted((countFirst, countSecond)=>countFirst-countSecond);
+    return sortedByAscendingFriends;
+}
+
+
+console.log(
+    sortByDescendingFriendCount([
+      {
+        name: "Moore Hensley",
+        friends: ["Sharron Pace"],
+        gender: "male"
+      },
+      {
+        name: "Sharlene Bush",
+        friends: ["Briana Decker", "Sharron Pace"],
+        gender: "female"
+      },
+      {
+        name: "Ross Vazquez",
+        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+        gender: "male"
+      },
+      {
+        name: "Elma Head",
+        friends: ["Goldie Gentry", "Aisha Tran"],
+        gender: "female"
+      },
+      {
+        name: "Carey Barr",
+        friends: ["Jordan Sampson", "Eddie Strong"],
+        gender: "male"
+      },
+      {
+        name: "Blackburn Dotson",
+        friends: ["Jacklyn Lucas", "Linda Chapman"],
+        gender: "male"
+      },
+      {
+        name: "Sheree Anthony",
+        friends: ["Goldie Gentry", "Briana Decker"],
+        gender: "female"
+      }
+    ])
+  );
+  // [
+  //   {
+  //     name: "Ross Vazquez",
+  //     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+  //     gender: "male"
+  //   },
+  //   {
+  //     name: "Sharlene Bush",
+  //     friends: ["Briana Decker", "Sharron Pace"],
+  //     gender: "female"
+  //   },
+  //   {
+  //     name: "Elma Head",
+  //     friends: ["Goldie Gentry", "Aisha Tran"],
+  //     gender: "female"
+  //   },
+  //   {
+  //     name: "Carey Barr",
+  //     friends: ["Jordan Sampson", "Eddie Strong"],
+  //     gender: "male"
+  //   },
+  //   {
+  //     name: "Blackburn Dotson",
+  //     friends: ["Jacklyn Lucas", "Linda Chapman"],
+  //     gender: "male"
+  //   },
+  //   {
+  //     name: "Sheree Anthony",
+  //     friends: ["Goldie Gentry", "Briana Decker"],
+  //     gender: "female"
+  //   },
+  //   {
+  //     name: "Moore Hensley",
+  //     friends: ["Sharron Pace"],
+  //     gender: "male"
+  //   }
+  // ]
