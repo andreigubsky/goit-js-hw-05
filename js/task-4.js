d3434f7996045cb2ac82545219917331cf5cc6ff
Andrew Gubsky <andrei.gubsky@gmail.com>
2025-09-10 11:12:35 +0300

    Task 4 is ready

--- js/task-4.js
+++ js/task-4.js
@@ -1,12 +1,23 @@
 /**
- * This function take users and sort them by decending friend count.
- * 
+ * @description This function take users and gender of user and calculate total balance by gender.
  * @param {Array<object>} users - Array of users.
- * @returns {Array<object>} - Array of users.
+ * @param {string} gender - Gender of user.
+ * @returns {number} - Sum of all balances.
  */
 
+// Напиши стрілочну функцію getTotalBalanceByGender(users, gender), яка прийматиме два параметра:
+
+// перший параметр users — масив обєктів користувачів,
+// другий параметр gender — рядок, що зберігає стать.
+// Функція має використовувати ланцюжок виклику методів та повертати загальний баланс користувачів 
+// (властивість balance), стать яких (властивість gender) збігається зі значенням параметра gender.
+
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. 
+// У консоль будуть виведені результати її роботи.
+
+
 const clients = [
-	{
+  {
     name: "Moore Hensley",
     gender: "male",
     balance: 2811
@@ -43,8 +54,13 @@ const clients = [
   }
 ];
 
-const getTotalBalanceByGender = (users, gender)=>{
-    
+const getTotalBalanceByGender = (users, gender) => {
+  const balances = users
+    .filter(user => user.gender === gender)
+    .reduce((totalBalance, user) => {
+      return totalBalance + user.balance;
+    }, 0);
+  return balances;
 }
 
 
